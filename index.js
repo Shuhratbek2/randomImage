a33f548c64a93ac65ed435571fdeb7bd235b9ec8
Sukhratbek <shuhratreyimboyevcoder@gmail.com>
2021-07-03 23:09:33 +0500

    2 server

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const bot = new Composer()
 bot.start((cxt) => {
     cxt.reply("salom men ishlayapman")
 });
+bot.help((cxt) => {
+    cxt.reply("salom sizga qanday yordam kerak")
+})
 
 module.exports = bot;
 // huhratreyimboyevcoder@gmail.com
 // obscure-meadow-83535
 // https://obscure-meadow-83535.herokuapp.com/
-// const token = "1778671799:AAFt7hY1iJDdwlbM7-eVA59YMsvF5YOMcu4"
