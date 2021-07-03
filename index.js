f3a7ef6f377782957e98f73acf4aa5e8b43f5b6e
Sukhratbek <shuhratreyimboyevcoder@gmail.com>
2021-07-03 23:01:09 +0500

    1 server

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Composer } = require('micro-bot')
+
+const bot = new Composer()
+
+
+bot.start((cxt) => {
+    cxt.reply("salom men ishlayapman")
+});
+
+module.exports = bot;
+// huhratreyimboyevcoder@gmail.com
+// obscure-meadow-83535
+// https://obscure-meadow-83535.herokuapp.com/
+// const token = "1778671799:AAFt7hY1iJDdwlbM7-eVA59YMsvF5YOMcu4"
